522d101678a0867e2f60826c2899552230c16b26
WilsonReese <71947861+WilsonReese@users.noreply.github.com>
2024-07-12 22:13:36 +0000

    not working but I am going to commit this so I can reference the files I want in a fresh RoRs app

--- config/application.rb
+++ config/application.rb
@@ -19,24 +19,20 @@ Bundler.require(*Rails.groups)
 
 module BattleBets
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w(tasks))
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
-
-    # Enable API-only mode
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
 
     config.generators do |g|
       g.test_framework nil
@@ -49,7 +45,6 @@ module BattleBets
 
     config.action_controller.default_protect_from_forgery = false
 
-    # Don't generate system test files.
     config.generators.system_tests = nil
   end
 end
---
+++ config/initializers/cors.rb
@@ -0,0 +1,11 @@
+# config/initializers/cors.rb
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*' # You can specify the allowed origins here, '*' allows all origins
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
