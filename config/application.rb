549ffb37b17c5dae3da29bf38ddd839bfc5f35e0
WilsonReese <71947861+WilsonReese@users.noreply.github.com>
2024-07-12 20:21:15 +0000

    changed application.rb file

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,23 @@
 require_relative "boot"
 
-require "rails/all"
+# Only include the frameworks you need for an API-only application
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_mailbox/engine"
+require "action_text/engine"
+require "action_view/railtie" # Optional, include if using view rendering in your API
+require "action_cable/engine"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RailsTemplate
+module BattleBets
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
@@ -23,6 +34,10 @@ module RailsTemplate
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Enable API-only mode
+    config.api_only = true
+
     config.generators do |g|
       g.test_framework nil
       g.factory_bot false
